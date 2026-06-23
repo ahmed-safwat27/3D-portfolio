15a38a943eeb8eb36b900bf120c7d45ef03b4237
Ahmed Safwat <ahmedsafwat27as@gmail.com>
2026-06-24 02:20:03 +0300

    First upload

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,27 @@
+import { mySocials } from "../constants";
+const Footer = () => {
+  return (
+    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+      <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      <div className="flex gap-2">
+        <p>Terms & Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+      <div className="flex gap-3">
+        {mySocials.map((social) => (
+          <a
+            key={social.name}
+            href={social.href}
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={social.icon} alt={social.name} />
+          </a>
+        ))}{" "}
+      </div>
+      <p>© 2026 Ahmed Safwat. All rights reserved.</p>
+    </section>
+  );
+};
+
+export default Footer;
